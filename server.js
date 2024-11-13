64c2804b5eca9be7772b99d0f8742613f1ce7952
usk0318 <saikiranuppalapati2@gmail.com>
2024-11-13 15:38:08 +0530

    .

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 
-const routs = require('./routs');
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Welcome to the home page!');
 });
 
-app.use('/api', routs);
 
 app.listen(8001, () => {
     console.log('Server is running on http://localhost:8001');
