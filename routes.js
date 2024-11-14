506021bb1fbe56b42d9c2305fe10ba617c20e8e6
usk0318 <saikiranuppalapati2@gmail.com>
2024-11-14 17:05:36 +0530

    .

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // Example route
 router.get('/', (req, res) => {
-    res.send('Welcome to the home page!');
+    res.send('welcome to api page');
 });
 
 // Another example route
