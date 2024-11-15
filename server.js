1e2802a36acfc3bad6f4a95bbdaf8ffd68b27075
usk0318 <saikiranuppalapati2@gmail.com>
2024-11-15 16:37:43 +0530

    .

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
-const routs = require('./routes');
 
 app.get('/', (req, res) => {
     res.send('Welcome to the API');
 });
-
-app.use('/api', routs);
-
 
 
 app.listen(8001, () => {
