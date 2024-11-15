89551866d74497a9b5aba71e6c579804d518c2c8
usk0318 <saikiranuppalapati2@gmail.com>
2024-11-15 16:40:49 +0530

    .

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.post('/post', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
