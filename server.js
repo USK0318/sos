32130acf9a4942e5baa8d5319e5dfb19dc65a9ec
usk0318 <saikiranuppalapati2@gmail.com>
2024-11-15 16:39:28 +0530

    .

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the API');
+app.post('/post', (req, res) => {
+    res.json({ message: 'Hello World!' });
 });
 
+app.get('/get', (req, res) => {
+    res.json({ message: 'Hello World!' });
+});
 
 app.listen(8001, () => {
     console.log('Server is running on http://localhost:8001');
